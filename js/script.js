a4438b7ff92b4e90199e337276565613d9a849c7
kanggun1111-rgb <kanggun1111@gmail.com>
2026-01-30 17:04:12 +0900

    Update whatever

--- js/script.js
+++ js/script.js
@@ -24,16 +24,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 
-// 뉴스 썸네일 클릭 시 메인 이미지 변경
-const mainNews = document.getElementById('news-display');
-const thumbs = document.querySelectorAll('#news-thumbnails img');
-
-thumbs.forEach(thumb => {
-    thumb.addEventListener('click', () => {
-        mainNews.src = thumb.src;
-    });
-});
-
 // Hero 비디오 강제 재생 (브라우저 정책 대비)
 const heroVideo = document.querySelector('.bg-video');
 heroVideo.muted = true;
